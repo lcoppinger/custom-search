86baaa6dc7d360e262b0f7c282b1b447218e5920
Lisa Coppinger <lisa.c.coppinger@uk.pwc.com>
2016-09-20 13:06:20 +0100

    test with simple control panel search

---
+++ dist/js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by 915128 on 9/20/16.
+ */
+
+$(document).ready(function(){
+  $(".overlay").fadeOut();
+  $(".search").click(function(){
+    $(this).blur();
+    $(".overlay").fadeIn();
+    $(".gsc-input").focus();
+  });
+
+  $(".close").click(function(){
+    $(".overlay").fadeOut();
+  });
+});
